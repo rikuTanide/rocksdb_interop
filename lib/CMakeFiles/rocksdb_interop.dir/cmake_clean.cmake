bab491c0e65f0b829b0ca2a06a89f05606e96f98
Ubuntu <ubuntu@ip-172-31-17-23.ap-northeast-1.compute.internal>
2020-01-13 07:16:49 +0000

    ビルドしなおした

---
+++ lib/CMakeFiles/rocksdb_interop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rocksdb_interop.dir/rocksdb_interop.cpp.o"
+  "librocksdb_interop.pdb"
+  "librocksdb_interop.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rocksdb_interop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
